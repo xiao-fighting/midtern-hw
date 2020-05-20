d86c7c6a34f05d3cd9ad0ac43f88b3965d873959
xiao-fighting <61406400+xiao-fighting@users.noreply.github.com>
2020-05-20 14:54:18 +0800

    test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import linebot from 'linebot'
 import dotenv from 'dotenv'
 
 import rp from 'request-promise'
+// import cheerio from 'cheerio'
 
 // 讀取.env檔
 dotenv.config()
@@ -25,13 +26,40 @@ const bot = linebot({
 // 當收到訊息時
 // 回傳 會是  the JSON string in the response  (json: true)
 bot.on('message', async (event) => {
-  let msg = ''
+  let msg = []
+  // const temp = []
   try {
-    const data = await rp({ url: 'https://kktix.com/events.json', json: true })
-    msg = data.entry[0].title
+    const data = await rp({ url: 'https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvOutdoorEdu.aspx', json: true })
+    // console.log(data.length)
+
+    if (event.message.type === 'text') {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].County.includes(event.message.text)) {
+          msg.push({ type: 'text', text: data[i].FarmNm_CH + '\n' + data[i].WebURL + '\n' + '欲前往農場地點則請輸入農場名稱' })
+          // msg.push({
+          //   type: 'location',
+          //   title: data[i].FarmNm_CH,
+          //   address: data[i].Address_CH,
+          //   latitude: data[i].Latitude,
+          //   longitude: data[i].Longitude
+          // })
+        } else if (data[i].FarmNm_CH.includes(event.message.text)) {
+          msg.push({
+            type: 'location',
+            title: data[i].FarmNm_CH,
+            address: data[i].Address_CH,
+            latitude: data[i].Latitude,
+            longitude: data[i].Longitude
+          })
+        }
+      }
+    } else {
+      msg = '請輸入文字'
+    }
   } catch (error) {
     msg = '發生錯誤'
   }
+  console.log(msg)
   event.reply(msg)
 })
 
